f7a728228cbc1916ffeeb1ef0397ad7c637b0492
Denis <denkv@users.noreply.github.com>
2022-11-11 12:20:35 +0100

    Use `env python3`

--- naive-eamt/start_docker_containers.sh
+++ naive-eamt/start_docker_containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "${BASH_SOURCE%/*}" || exit
 # find the relevant modules for compose profile e.g COMPOSE_PROFILES=mag,profile2
-export COMPOSE_PROFILES=`python find_profiles.py`
+export COMPOSE_PROFILES=`./find_profiles.py`
 echo 'These are the active profiles: '$COMPOSE_PROFILES
 docker-compose up -d || docker compose up -d
