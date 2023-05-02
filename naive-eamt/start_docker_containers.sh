b362797dbe38dc783fecea9ba5348c41e2d64e9e
Nikit Srivastava <nikit91@gmail.com>
2023-05-02 12:18:31 +0200

    adding --build to docker compose

--- naive-eamt/start_docker_containers.sh
+++ naive-eamt/start_docker_containers.sh
@@ -3,4 +3,4 @@ cd "${BASH_SOURCE%/*}" || exit
 # find the relevant modules for compose profile e.g COMPOSE_PROFILES=mag,profile2
 export COMPOSE_PROFILES=`./find_profiles.py`
 echo 'These are the active profiles: '$COMPOSE_PROFILES
-docker compose up -d
+docker compose up --build -d
