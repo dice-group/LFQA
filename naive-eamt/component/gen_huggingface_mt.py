863b3060ac44f9a33074f099fb0a35f4faad2072
Nikit Srivastava <nikit91@gmail.com>
2023-05-25 09:53:41 +0200

    generalising huggingface based mt components

---
+++ naive-eamt/component/gen_huggingface_mt.py
@@ -0,0 +1,28 @@
+# This class demonstrates how each component should look like
+import logging
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+from mt_abs import GenMT
+
+class GenHuggingfaceMt(GenMT):
+    def __init__(self, model_name, tokenizer_name, model_kwargs, tokenizer_kwargs, lang_code_map):
+        """
+        Load the resources needed for your component onto the memory only in this block.
+        It helps keep the framework from unnecessarily occupying the memory.
+        """
+        # Only accessible inside the docker network
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, **model_kwargs)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(tokenizer_name, **tokenizer_kwargs)
+        self.lang_code_map = lang_code_map
+
+        logging.debug('%s component initialized.' % type(self).__name__)
+    
+    def translate_text(self, trans_text, source_lang, target_lang, extra_args):
+        self.tokenizer.src_lang = self.lang_code_map[source_lang]
+        encoded_ar = self.tokenizer(trans_text, return_tensors="pt")
+        generated_tokens = self.model.generate(
+            **encoded_ar,
+            forced_bos_token_id=self.tokenizer.lang_code_to_id[self.lang_code_map[target_lang]]
+        )
+        trans_text = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+        return trans_text
--- naive-eamt/component/mbart_plc_ft_mt.py
+++ naive-eamt/component/mbart_plc_ft_mt.py
@@ -1,27 +1,14 @@
-# This class demonstrates how each component should look like
-import logging
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
-
 from mbart_mt import MbartMt
+from gen_huggingface_mt import GenHuggingfaceMt
+import copy
 
-class MbartPlcFtMt(MbartMt):
-    mbart_model_name = "/neamt/ft_models/mbart50_mintaka_plc_final"
-    mbart_tokenizer_name = "/neamt/ft_models/mbart50_mintaka_plc_final"
+
+class MbartPlcFtMt(GenHuggingfaceMt):
+    init_kwargs = copy.deepcopy(MbartMt.init_kwargs)
+
     def __init__(self):
-        """
-        Load the resources needed for your component onto the memory only in this block.
-        It helps keep the framework from unnecessarily occupying the memory.
-        """
-        # Only accessible inside the docker network
-        self.model = MBartForConditionalGeneration.from_pretrained(self.mbart_model_name, local_files_only=True)
-        self.tokenizer = MBart50TokenizerFast.from_pretrained(self.mbart_tokenizer_name, local_files_only=True)
-        self.lang_code_map = {
-            'en': 'en_XX',
-            'de': 'de_DE',
-            'ru': 'ru_RU',
-            'fr': 'fr_XX',
-            'es': 'es_XX',
-            'pt': 'pt_XX',
-            'it': 'it_IT'
-        }
-        logging.debug('MbartMt (fine-tuned) component initialized.')
+        self.init_kwargs["model_name"] = "/neamt/ft_models/mbart50_mintaka_plc_final"
+        self.init_kwargs["tokenizer_name"] = "/neamt/ft_models/mbart50_mintaka_plc_final"
+        self.init_kwargs["model_kwargs"] = {"local_files_only": True}
+        self.init_kwargs["tokenizer_kwargs"] = {"local_files_only": True}
+        super(MbartPlcFtMt, self).__init__(**self.init_kwargs)
