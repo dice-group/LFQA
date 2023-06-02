89742fd92aeb39383d8f618a74f1ec8c93f48c49
Nikit Srivastava <nikit91@gmail.com>
2023-06-02 21:00:09 +0200

    adding placeholder fine-tuned opusmt

--- naive-eamt/start.py
+++ naive-eamt/start.py
@@ -34,6 +34,7 @@ from swc_ner_el import SwcNerEl
 from mbart_ep_mt import MbartEpMt
 from mbart_plc_ft_mt import MbartPlcFtMt
 from nllb_plc_ft_mt import NllbPlcFtMt
+from opus_plc_ft_mt import OpusPlcFtMt
 
 sys.path.insert(1, '/neamt/util/')
 import stats_util
@@ -59,7 +60,8 @@ comp_map = {
     'swc_ner_el': SwcNerEl,
     'mbart_ep_mt': MbartEpMt,
     'mbart_plc_ft_mt': MbartPlcFtMt,
-    'nllb_plc_ft_mt': NllbPlcFtMt
+    'nllb_plc_ft_mt': NllbPlcFtMt,
+    'opus_plc_ft_mt': OpusPlcFtMt
 }
 
 def_placeholder = '00'
