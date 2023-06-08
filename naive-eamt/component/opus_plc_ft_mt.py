ced394d7717b56561173a7d22c1ab3a6a363459e
Nikit Srivastava <nikit91@gmail.com>
2023-06-08 13:52:46 +0200

    optimizing ft opus for parallel access

--- naive-eamt/component/opus_plc_ft_mt.py
+++ naive-eamt/component/opus_plc_ft_mt.py
@@ -21,6 +21,8 @@ class OpusPlcFtMt(GenMT):
     }
 
     model_tok_map = {}
+
+    TOKENIZER = {}
     def __init__(self):
         """
         Load the resources needed for your component onto the memory only in this block.
@@ -33,16 +35,30 @@ class OpusPlcFtMt(GenMT):
             tokenizer_name = self.init_args["tokenizer_name_template"] % lang
             tokenizer_kwargs = self.init_args["tokenizer_kwargs"]
             # initializing tokenizer and model
-            tokenizer = AutoTokenizer.from_pretrained(model_name, **model_kwargs)
-            model = AutoModelForSeq2SeqLM.from_pretrained(tokenizer_name, **tokenizer_kwargs)
-            self.model_tok_map[lang] = (model, tokenizer)
+            model = AutoModelForSeq2SeqLM.from_pretrained(model_name, **model_kwargs)
+            # tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, **tokenizer_kwargs)
+            self.model_tok_map[lang] = (model, (tokenizer_name, tokenizer_kwargs))
 
         logging.debug('%s component initialized.' % type(self).__name__)
-    
+
+    """
+    Huggingface's tokenizers have an issue with parallel thread access (https://github.com/huggingface/tokenizers/issues/537).
+    Implementing a workaround mentioned in: https://github.com/huggingface/tokenizers/issues/537#issuecomment-1372231603    
+    """
+    def get_tokenizer(self, lang):
+        _id = threading.get_ident()
+        tokenizer = self.TOKENIZER.get(_id, None)
+        if tokenizer is None:
+            tok_info = self.model_tok_map[lang]
+            tokenizer_name = tok_info[0]
+            tokenizer_kwargs = tok_info[1]
+            tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, **tokenizer_kwargs)
+            self.TOKENIZER[_id] = tokenizer
+        return tokenizer
     def translate_text(self, trans_text, source_lang, target_lang, extra_args):
         # fetch model and tokenizer
         model = self.model_tok_map[source_lang][0]
-        tokenizer = self.model_tok_map[source_lang][1]
+        tokenizer = self.get_tokenizer(source_lang)
 
         tokenizer.src_lang = self.lang_code_map[source_lang]
         encoded_ar = tokenizer(trans_text, return_tensors="pt")
