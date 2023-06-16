9fafa80ed44442e2c45ebe164530d3158d933e8b
Denis <denkv@users.noreply.github.com>
2023-06-16 17:48:24 +0200

    Ignore `owl:` URIs

--- naive-eamt/eval/analysis.py
+++ naive-eamt/eval/analysis.py
@@ -137,6 +137,7 @@ def resolve(link):
     or link.startswith('http://dbpedia.org/class/') \
     or link.startswith('http://www.w3.org/1999/02/22-rdf-syntax-ns#') \
     or link.startswith('http://www.w3.org/2000/01/rdf-schema#') \
+    or link.startswith('http://www.w3.org/2002/07/owl#') \
     or link.startswith('http://www.w3.org/2004/02/skos/core#') \
     or link.startswith('http://purl.org/dc/terms/') \
     or link.startswith('http://xmlns.com/foaf/0.1/') \
