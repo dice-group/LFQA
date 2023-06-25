d6a233c432a6e475748015acaa3addfc4c83dcdc
KshitijS149 <shettykshitij149@gmail.com>
2023-06-25 10:22:28 +0200

    Updated new_setup.sh

--- naive-eamt/new_setup.sh
+++ naive-eamt/new_setup.sh
@@ -17,44 +17,54 @@ echo 'MAG: Downloading Indexes for en, de, fr & es..'
 download_files() {
   readarray -d / -t file_name <<< "$1"
   readarray -d . -t file_name <<< "${file_name[-1]}"
-  
+  #file_ext="${file_name[-1]}"
+  read -a file_ext <<< "${file_name[-1]}"
+  #echo "$file_name.$file_ext"
   file_name="$2/$file_name"
   #echo "$file_name"
-  if [[ ! -d "$file_name" ]] && [[ -f "$file_name.zip"]]
+  if [[ ! -d "$file_name" ]] && [[ -f "$file_name.$file_ext" ]]
   then
-    echo "$file_name.zip is present"
-    echo "$Unzipping"
-    unzip "$file_name.zip" -d "$2"
-    rm "$file_name.zip"
-  elif [[ ! -f "$file_name.zip" ]] && [[ ! - d "$file_name"]]
+    echo "$file_name.$file_ext is present"
+    #echo "Unzipping"
+    #echo "$file_name"
+    if [[ "$file_ext" = "zip" ]]
+    then
+      unzip "$file_name.zip" -d "$2"
+      rm "$file_name.zip"
+    fi
+  elif [[ ! -f "$file_name.$file_ext" ]] && [[ ! -d "$file_name" ]]
   then
-    echo "$file_name.zip is not present"
-    wget -O "$file_name".zip "$1"
-    echo "$Unzipping"
-    unzip "$file_name.zip" -d "$2"
-    rm "$file_name.zip"
+    echo "$file_name.$file_ext is not present"
+    echo "$file_ext"
+    wget -O "$file_name.$file_ext" "$1"
+    #echo "Unzipping"
+    if [[ "$file_ext" = "zip" ]]
+    then
+      unzip "$file_name.zip" -d "$2"
+      rm "$file_name.zip"
+    fi
   else
-    echo "$file_name.zip is already unzipped"
+    echo "$file_name.$file_ext is already unzipped"
   fi
 }
 
 download_files "http://hobbitdata.informatik.uni-leipzig.de/agdistis/dbpedia_index_2016-04/en/indexdbpedia_en_2016.zip" "data/mag_data"
 
-
 file_name=data/mag_data/index_bycontext
-#echo "$file_name"
-if [[ ! -d "$file_name_en" ]] && [[ -f "$file_name.zip"]]
+echo "$file_name"
+if [[ ! -d "$file_name"_en ]] && [[ -f "$file_name.zip" ]]
 then
   echo "$file_name.zip is present"
-  echo "$Unzipping"
-  unzip "$file_name.zip" -d data/mag_data
+  echo "$file_name".zip
+  echo "Unzipping"
+  unzip "$file_name".zip -d data/mag_data
   mv data/mag_data/index_bycontext data/mag_data/index_bycontext_en
   rm "$file_name.zip"
-elif [[ ! -f "$file_name.zip" ]] && [[ ! - d "$file_name_en"]]
+elif [[ ! -f "$file_name.zip" ]] && [[ ! -d "$file_name"_en ]]
 then
   echo "$file_name.zip is not present"
   wget -O "$file_name".zip http://hobbitdata.informatik.uni-leipzig.de/agdistis/dbpedia_index_2016-04/en/index_bycontext.zip
-  echo "$Unzipping"
+  echo "Unzipping"
   unzip "$file_name.zip" -d data/mag_data
   mv data/mag_data/index_bycontext data/mag_data/index_bycontext_en
   rm "$file_name.zip"
@@ -74,18 +84,18 @@ download_files "http://hobbitdata.informatik.uni-leipzig.de/agdistis/dbpedia_ind
 
 file_name=data/mag_data/index_bycontext
 #echo "$file_name"
-if [[ ! -d "$file_name_de" ]] && [[ -f "$file_name.zip"]]
+if [[ ! -d "$file_name"_de ]] && [[ -f "$file_name.zip" ]]
 then
   echo "$file_name.zip is present"
-  echo "$Unzipping"
+  echo "Unzipping"
   unzip "$file_name.zip" -d data/mag_data
   mv data/mag_data/index_bycontext data/mag_data/index_bycontext_de
   rm "$file_name.zip"
-elif [[ ! -f "$file_name.zip" ]] && [[ ! - d "$file_name_de"]]
+elif [[ ! -f "$file_name.zip" ]] && [[ ! -d "$file_name"_de ]]
 then
   echo "$file_name.zip is not present"
   wget -O "$file_name".zip http://hobbitdata.informatik.uni-leipzig.de/agdistis/dbpedia_index_2016-04/de/index_bycontext.zip
-  echo "$Unzipping"
+  echo "Unzipping"
   unzip "$file_name.zip" -d data/mag_data
   mv data/mag_data/index_bycontext data/mag_data/index_bycontext_de
   rm "$file_name.zip"
@@ -123,7 +133,23 @@ echo 'MAG: Finished!'
 #cd ..
 
 # download fasttext language classification model
-download_files "wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.ftz" "data"
+nonzip() {
+  readarray -d / -t file_name <<< "$1"
+  #readarray -d . -t file_name <<< "${file_name[-1]}"
+  #file_ext="${file_name[-1]}"
+  read -a file_name <<< "${file_name[-1]}"
+  #echo "$file_name.$file_ext"
+  file_name="$2/$file_name"
+  echo "$file_name"
+  if [[ ! -f "$file_name" ]]
+  then
+    echo "$file_name is not present"
+    wget -O "$file_name" "$1"
+  else
+    echo "$file_name is present"
+  fi
+}
+nonzip "wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.ftz" "data"
 #wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.ftz
 # download libre repo
 cd data
@@ -148,60 +174,80 @@ cd Opus-MT
 # Copy customized services.json
 mv services.json services.json.old
 cp ../../helsinki_opusmt_services.json ./services.json
-exit
-mkdir models && cd "$_"
-# Download models
-mkdir de-en && cd "$_"
-wget https://object.pouta.csc.fi/OPUS-MT-models/de-en/opus-2020-02-26.zip
-unzip 'opus-2020-02-26.zip'
-rm -rf 'opus-2020-02-26.zip'
+
+
+# Download and extract models
+mkdir -p models
+cd models
+
+declare -A model_urls=(
+  ["de-en"]="https://object.pouta.csc.fi/OPUS-MT-models/de-en/opus-2020-02-26.zip"
+  ["es-en"]="https://object.pouta.csc.fi/OPUS-MT-models/es-en/opus-2019-12-04.zip"
+  ["fr-en"]="https://object.pouta.csc.fi/OPUS-MT-models/fr-en/opus-2020-02-26.zip"
+  ["ru-en"]="https://object.pouta.csc.fi/OPUS-MT-models/ru-en/opus-2020-02-26.zip"
+  ["nl-en"]="https://object.pouta.csc.fi/OPUS-MT-models/nl-en/opus-2019-12-05.zip"
+  ["zh-en"]="https://object.pouta.csc.fi/Tatoeba-MT-models/zho-eng/opus-2020-07-14.zip"
+  ["it-en"]="https://object.pouta.csc.fi/OPUS-MT-models/it-en/opus-2019-12-05.zip"
+  ["pt-en"]="https://object.pouta.csc.fi/OPUS-MT-models/pt-en/opus-2019-12-05.zip"
+)
+
+# Download and extract the models
+for lang_pair in "${!model_urls[@]}"; do
+  if [[ ! -d "$lang_pair" ]]
+  then
+    mkdir -p "$lang_pair" && cd "$lang_pair" 
+    model_url="${model_urls[$lang_pair]}"
+    model_zip="${model_url##*/}"
+    wget "$model_url"
+    unzip -n "$model_zip"
+    rm -f "$model_zip"
+    cd ..
+  fi 
+ 
+done
+
+
 cd ..
-mkdir es-en && cd "$_"
-wget https://object.pouta.csc.fi/OPUS-MT-models/es-en/opus-2019-12-04.zip
-unzip 'opus-2019-12-04.zip'
-rm -rf 'opus-2019-12-04.zip'
-cd ..
-mkdir fr-en && cd "$_"
-wget https://object.pouta.csc.fi/OPUS-MT-models/fr-en/opus-2020-02-26.zip
-unzip 'opus-2020-02-26.zip'
-rm -rf 'opus-2020-02-26.zip'
-cd ..
-mkdir ru-en && cd "$_"
-wget https://object.pouta.csc.fi/OPUS-MT-models/ru-en/opus-2020-02-26.zip
-unzip 'opus-2020-02-26.zip'
-rm -rf 'opus-2020-02-26.zip'
-cd ..
-mkdir nl-en && cd "$_"
-wget https://object.pouta.csc.fi/OPUS-MT-models/nl-en/opus-2019-12-05.zip
-unzip 'opus-2019-12-05.zip'
-rm -rf 'opus-2019-12-05.zip'
-cd ..
-mkdir zh-en && cd "$_"
-wget https://object.pouta.csc.fi/Tatoeba-MT-models/zho-eng/opus-2020-07-14.zip
-unzip 'opus-2020-07-14.zip'
-rm -rf 'opus-2020-07-14.zip'
-cd ..
-mkdir it-en && cd "$_"
-wget https://object.pouta.csc.fi/OPUS-MT-models/it-en/opus-2019-12-05.zip
-unzip 'opus-2019-12-05.zip'
-rm -rf 'opus-2019-12-05.zip'
-cd ..
-mkdir pt-en && cd "$_"
-wget https://object.pouta.csc.fi/OPUS-MT-models/pt-en/opus-2019-12-05.zip
-unzip 'opus-2019-12-05.zip'
-rm -rf 'opus-2019-12-05.zip'
-cd ../..
-docker build . -t opus-mt
+
+# Build Docker image
+
+
+#(Check if group exists)
+#groupadd docker
+#usermod -aG docker "${USER}"
+
+#su -s (logout and log back in)
+#docker build . -t opus-mt
+
+
+#cd ../..
+#docker build . -t opus-mt
 cd ..
 # download genre data
 # Downloading models
-mkdir mgenre_data && cd "$_"
-wget https://dl.fbaipublicfiles.com/GENRE/fairseq_multilingual_entity_disambiguation.tar.gz
+echo "$(pwd)"
+if [[ ! -d mgenre_data ]]
+then
+  mkdir -p mgenre_data 
+fi
+
+
 # untar
-tar -xvf fairseq_multilingual_entity_disambiguation.tar.gz
-wget http://dl.fbaipublicfiles.com/GENRE/titles_lang_all105_trie_with_redirect.pkl
-wget https://dl.fbaipublicfiles.com/GENRE/lang_title2wikidataID-normalized_with_redirect.pkl
-wget http://dl.fbaipublicfiles.com/GENRE/titles_lang_all105_marisa_trie_with_redirect.pkl
+if [[ -f mgenre_data/fairseq_multilingual_entity_disambiguation.tar.gz ]]
+then
+  tar -xvf "mgenre_data/fairseq_multilingual_entity_disambiguation.tar.gz" -C mgenre_data
+  rm mgenre_data/fairseq_multilingual_entity_disambiguation.tar.gz
+elif [[ ! -d mgenre_data/fairseq_multilingual_entity_disambiguation ]]
+then
+  nonzip "https://dl.fbaipublicfiles.com/GENRE/fairseq_multilingual_entity_disambiguation.tar.gz" "mgenre_data"
+  tar -xvf "mgenre_data/fairseq_multilingual_entity_disambiguation.tar.gz" -C mgenre_data
+  rm mgenre_data/fairseq_multilingual_entity_disambiguation.tar.gz
+else
+  echo "mgenre_data/fairseq_multilingual_entity_disambiguation.tar.gz has been extracted"
+fi
+nonzip "http://dl.fbaipublicfiles.com/GENRE/titles_lang_all105_trie_with_redirect.pkl" "mgenre_data"
+nonzip "https://dl.fbaipublicfiles.com/GENRE/lang_title2wikidataID-normalized_with_redirect.pkl" "mgenre_data"
+nonzip "http://dl.fbaipublicfiles.com/GENRE/titles_lang_all105_marisa_trie_with_redirect.pkl" "mgenre_data"
 # Download evaluation data
 #cd ../../eval
 #mkdir qald10 && cd "$_"
