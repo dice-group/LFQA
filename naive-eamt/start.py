bcf227db789b66a2baf166f8ccf1b4bee868f804
Nikit Srivastava <nikit91@gmail.com>
2023-07-25 15:53:13 +0200

    adding logic for json data in request

--- naive-eamt/start.py
+++ naive-eamt/start.py
@@ -264,7 +264,14 @@ def cus_pipe():
     global stat_dict
     # incrementing query count
     stat_dict['query_count'] += 1
-    data = request.form
+
+    #debug
+    logging.debug(request)
+
+    if request.get('form'):
+        data = request.get('form')
+    elif request.get('json'):
+        data = request.get('json')
     logging.info('Query received at custom-pipeline')
     logging.info('Data received for translation: %s' % data)
     comp_arr = data['components'].split(',')
