489fd5d33665f0d9885222e0ac27cce37899cf71
Denis <denkv@users.noreply.github.com>
2023-09-11 15:04:25 +0200

    Add average_analysis.py calls into the run script

--- naive-eamt/eval/run_analysis.sh
+++ naive-eamt/eval/run_analysis.sh
@@ -22,3 +22,16 @@ args="$globalargs --path $base/mintaka --dataset-format=mintaka --dataset-file $
 ./analysis.py --metric sacrebleu $args
 ./analysis.py --metric entitiesfound $args
 ./analysis.py --metric labels $args
+
+./average_analysis.py --path $base/qald9plustest --problematicity-file $HOME/.local/share/datasets/qald-9-plus-test-query-entities.json.distinctlabels --classes-file $HOME/.local/share/datasets/qald-9-plus-test-query-entities.json.classes --problematicity-threshold 0.0
+./average_analysis.py --path $base/qald9plustest --problematicity-file $HOME/.local/share/datasets/qald-9-plus-test-query-entities.json.distinctlabels --classes-file $HOME/.local/share/datasets/qald-9-plus-test-query-entities.json.classes --problematicity-threshold 0.5
+./average_analysis.py --path $base/qald9plustest --problematicity-file $HOME/.local/share/datasets/qald-9-plus-test-query-entities.json.distinctlabels --classes-file $HOME/.local/share/datasets/qald-9-plus-test-query-entities.json.classes --problematicity-threshold 1.0
+./average_analysis.py --path $base/qald9plustrain --problematicity-file $HOME/.local/share/datasets/qald-9-plus-train-query-entities.json.distinctlabels --classes-file $HOME/.local/share/datasets/qald-9-plus-train-query-entities.json.classes --problematicity-threshold 0.0
+./average_analysis.py --path $base/qald9plustrain --problematicity-file $HOME/.local/share/datasets/qald-9-plus-train-query-entities.json.distinctlabels --classes-file $HOME/.local/share/datasets/qald-9-plus-train-query-entities.json.classes --problematicity-threshold 0.5
+./average_analysis.py --path $base/qald9plustrain --problematicity-file $HOME/.local/share/datasets/qald-9-plus-train-query-entities.json.distinctlabels --classes-file $HOME/.local/share/datasets/qald-9-plus-train-query-entities.json.classes --problematicity-threshold 1.0
+./average_analysis.py --path $base/qald10 --problematicity-file $HOME/.local/share/datasets/qald-10-query-entities.json.distinctlabels --problematicity-threshold 0.0 --classes-file $HOME/.local/share/datasets/qald-10-query-entities.json.classes
+./average_analysis.py --path $base/qald10 --problematicity-file $HOME/.local/share/datasets/qald-10-query-entities.json.distinctlabels --problematicity-threshold 0.5 --classes-file $HOME/.local/share/datasets/qald-10-query-entities.json.classes
+./average_analysis.py --path $base/qald10 --problematicity-file $HOME/.local/share/datasets/qald-10-query-entities.json.distinctlabels --problematicity-threshold 1.0 --classes-file $HOME/.local/share/datasets/qald-10-query-entities.json.classes
+./average_analysis.py --path $base/mintaka --problematicity-file $HOME/.local/share/datasets/Mintaka/v1.1/mintaka_dev.json.distinctlabels --problematicity-threshold 0.0 --classes-file $HOME/.local/share/datasets/Mintaka/v1.1/mintaka_dev.json.classes
+./average_analysis.py --path $base/mintaka --problematicity-file $HOME/.local/share/datasets/Mintaka/v1.1/mintaka_dev.json.distinctlabels --problematicity-threshold 0.5 --classes-file $HOME/.local/share/datasets/Mintaka/v1.1/mintaka_dev.json.classes
+./average_analysis.py --path $base/mintaka --problematicity-file $HOME/.local/share/datasets/Mintaka/v1.1/mintaka_dev.json.distinctlabels --problematicity-threshold 1.0 --classes-file $HOME/.local/share/datasets/Mintaka/v1.1/mintaka_dev.json.classes
